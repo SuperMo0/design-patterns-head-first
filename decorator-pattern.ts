f9a9f0e7a12336e89675758baac209b5f3304a33
mowafak <moofk2002@gmail.com>
2026-08-20 21:51:08 +0300

    update decorator pattern

--- decorator-pattern.ts
+++ decorator-pattern.ts
@@ -5,41 +5,78 @@
  *
  * 1. open-closed princples: adding new behavior involves making a new decorator
  *
- * 2. favor composition over inhertince: although the decorator extends the component class
- * it doesn't focus on the inherited behvior rather it's more focused on extedning behavior
- * by using delegation (delegation is just calling the method of the wrapped object)
+ * 2. favor composition over inhertince:
+ * although the decorator extends the component class
+ * it doesn't focus on the inherited behvior rather
+ * it's more focused on extedning behavior
+ * by using delegation
+ * (delegation is just calling the method of the wrapped object)
+ *
+ * We’re going to see a much better way of
+ * creating decorated objects when we cover the
+ * Factory and Builder Design Patterns.
  *
  */
 
+type CupSize = "small" | "large" | "xlarge";
 abstract class Beverage {
-  #description: string = "No description";
-  constructor() {}
+  #description: string;
+  #size: CupSize;
+  constructor(description: string, size: CupSize) {
+    this.#description = description;
+    this.#size = size;
+  }
   get description() {
-    return this.#description;
+    return this.#description + " " + this.#size;
   }
   set description(description) {
     this.#description = description;
+  }
+
+  get size() {
+    return this.#size;
+  }
+  set size(size: CupSize) {
+    this.#size = this.size;
   }
   abstract cost(): number;
 }
 
 class Latte extends Beverage {
-  constructor() {
-    super();
-    this.description = "Latte";
+  constructor(size: CupSize) {
+    super("Latte", size);
   }
   cost(): number {
-    return 12;
+    switch (this.size) {
+      case "small": {
+        return 12;
+      }
+      case "large": {
+        return 14;
+      }
+      case "xlarge": {
+        return 15;
+      }
+    }
   }
 }
 
 class Espresso extends Beverage {
-  constructor() {
-    super();
-    this.description = "Espresso";
+  constructor(size: CupSize) {
+    super("Espresso", size);
   }
-  cost() {
-    return 1.5;
+  cost(): number {
+    switch (this.size) {
+      case "small": {
+        return 12;
+      }
+      case "large": {
+        return 14;
+      }
+      case "xlarge": {
+        return 15;
+      }
+    }
   }
 }
 
@@ -47,8 +84,7 @@ abstract class BevarageDecorator extends Beverage {
   beverageComponent: Beverage;
 
   constructor(beverage: Beverage, description: string) {
-    super();
-    this.description = beverage.description + " + " + description;
+    super(beverage.description + " + " + description, beverage.size);
     this.beverageComponent = beverage;
   }
 }
@@ -56,8 +92,18 @@ class MochaDecorator extends BevarageDecorator {
   constructor(beverage: Beverage) {
     super(beverage, "Mocha");
   }
-  cost() {
-    return this.beverageComponent.cost() + 3;
+  cost(): number {
+    switch (this.size) {
+      case "small": {
+        return this.beverageComponent.cost() + 1;
+      }
+      case "large": {
+        return this.beverageComponent.cost() + 2;
+      }
+      case "xlarge": {
+        return this.beverageComponent.cost() + 3;
+      }
+    }
   }
 }
 
@@ -66,12 +112,21 @@ class WhipDecorator extends BevarageDecorator {
     super(beverage, "Whip");
     this.beverageComponent = beverage;
   }
-
   cost(): number {
-    return this.beverageComponent.cost() + 2;
+    switch (this.size) {
+      case "small": {
+        return this.beverageComponent.cost() + 1;
+      }
+      case "large": {
+        return this.beverageComponent.cost() + 2;
+      }
+      case "xlarge": {
+        return this.beverageComponent.cost() + 3;
+      }
+    }
   }
 }
 
-const cup = new WhipDecorator(new MochaDecorator(new Latte()));
+const cup = new WhipDecorator(new MochaDecorator(new Latte("large")));
 console.log(cup.description);
 console.log(cup.cost());
---
+++ readme.md
@@ -0,0 +1,8 @@
+# OO Design Principles
+
+## Principles
+
+1. Encapsulate what varies
+2. Design for an interface not for an implementation
+3. classes should be closed for modifiction but open for extension
+4. strive for a loosly coupled design between objects that interact
