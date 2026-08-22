dce676758f3a882fe49b0b45e388275bf23c8c81
mowafak <moofk2002@gmail.com>
2026-08-22 18:56:50 +0300

    add simple factory example

---
+++ simple-factory-pattern.ts
@@ -0,0 +1,53 @@
+/**
+ * according to Head first design patterns book
+ * `simple factory` is when we create our objects using another concrete object -lets call it `ObjectCreator`-
+ * and we use composotion to compose that `ObjectCreator` object with the class that needs it to create objects
+ *
+ * in the following examples the PizzaStore can be composed with any PizzaCreator
+ * in this example we have AmericanPizzaCreator but we might have ItalinPizzaCreator also
+ */
+
+type PizzaTypes = "cheese" | "chickn";
+abstract class Pizza {
+  description: string;
+  constructor(description: string) {
+    this.description = description;
+  }
+  serve() {
+    console.log(this.description);
+  }
+}
+interface PizzaCreator {
+  create(type: PizzaTypes): Pizza;
+}
+
+class AmericanCheesePiza extends Pizza {}
+class AmericanChickenPiza extends Pizza {}
+class AmericanPizzaCreator implements PizzaCreator {
+  // this is our ObjectCreator
+  create(type: PizzaTypes): Pizza {
+    if (type == "cheese") {
+      return new AmericanCheesePiza("American Cheese Pizza");
+    } else if (type == "chickn") {
+      return new AmericanChickenPiza("American Chicken Pizza");
+    } else {
+      throw Error("Unknown pizza!");
+    }
+  }
+}
+class PizzaStore {
+  pizzaCreator: PizzaCreator;
+
+  constructor(pizzaCreator: PizzaCreator) {
+    this.pizzaCreator = pizzaCreator;
+  }
+
+  createPizza(type: PizzaTypes) {
+    // this class doesn't know or care what creator he is dealing with
+    const pizza = this.pizzaCreator.create(type);
+    pizza.serve();
+  }
+}
+
+const AmericanPizzaStore = new PizzaStore(new AmericanPizzaCreator());
+AmericanPizzaStore.createPizza("cheese");
